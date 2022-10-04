247518b65d621af5edf35159d3ff7196c769b5cd
sgovind158 <govindsahu2842@gmail.com>
2022-10-04 21:53:26 +0530

    basic setup done

---
+++ worldtask/src/App.tsx
@@ -0,0 +1,13 @@
+import React from 'react';
+
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+     <span className='heading'>World Task</span>
+    </div>
+  );
+}
+
+export default App;
