5950bc0511711bc0a14c0fc3128fdeb6803a5931
sgovind158 <govindsahu2842@gmail.com>
2022-10-05 11:36:17 +0530

    todo app done

--- worldtask/src/componets/SingleTodo.tsx
+++ worldtask/src/componets/SingleTodo.tsx
@@ -1,4 +1,4 @@
-import React from 'react'
+import React, { useRef, useState ,useEffect} from 'react'
 import { Todo } from './models/model'
 import { AiFillEdit,AiFillDelete } from 'react-icons/ai';
 import { MdOutlineDone } from 'react-icons/md';
@@ -10,6 +10,14 @@ interface Props{
 
 const SingleTodo :React.FC<Props>= ({todos,todo,setTodos}) => {
 
+  const [edit,setEdit] = useState<boolean>(false)
+  const [editTodo,setEditTodo] = useState<string>(todo.todo)
+  const inputRef = useRef<HTMLInputElement>(null);
+
+  useEffect(()=>{
+    inputRef.current?.focus()
+
+  },[edit])
   const handleDone = (id:number)=>{
          setTodos(
           todos.map((todoObj)=>  todoObj.id === id ? {...todoObj,isDone : !todoObj.isDone } : todoObj
@@ -21,17 +29,41 @@ const SingleTodo :React.FC<Props>= ({todos,todo,setTodos}) => {
     let newTodos = todos.filter((todoObj)=> todoObj.id !== id)
     setTodos(newTodos)
   }
-  return (
-    <form className='todos_single'>
 
-     {todo.isDone ?(
+  const handleEdit = (e:React.FormEvent,id:Number)=>{
+        e.preventDefault()
+let editTodosObj = todos.map((todoObj)=> todoObj.id === id ?{...todoObj,todo : editTodo }:todoObj)
+        setTodos(editTodosObj )
+
+        setEdit(false)
+  }
+
+
+
+  return (
+    <form className='todos_single' onSubmit={(e)=>handleEdit(e,todo.id)}>
+{
+  edit ? (
+      <input
+          value={editTodo}
+          onChange={(e) => setEditTodo(e.target.value)}
+          className="todos_single_text"
+          ref={inputRef}
+        />
+  ) : todo.isDone ?(
       <s className='todos_single_text'>{todo.todo}</s>
      ) : (
 <span className='todos_single_text'>{todo.todo}</span>
      )}
       
       <div>
-        <span className='icon'>
+        <span className='icon'
+          onClick={() => {
+            if (!edit && !todo.isDone) {
+              setEdit(!edit);
+            }
+          }}
+        >
         <AiFillEdit/>
         </span>
         
